48164264cd7d415d217fcf0e802daf27937e1a2c
amine <aaliane781@gmail.com>
2023-12-02 18:38:35 +0100

    newf2

--- src/components/toggle.jsx
+++ src/components/toggle.jsx
@@ -3,15 +3,28 @@ import { useState } from "react";
 const toggle = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <div
-      class="toggle"
-      data-toggle={toggle}
-      onClick={() => setToggle((prevState) => !prevState)}
-    >
-      <span class="toggle__bar"></span>
-      <span class="toggle__bar"></span>
-      <span class="toggle__bar"></span>
-    </div>
+    <>
+      <ul class="nav__menu" data-toggle={toggle}>
+        <li class="nav__menu__item">
+          <a href="/">Home</a>
+        </li>
+        <li class="nav__menu__item">
+          <a href="/about/">About</a>
+        </li>
+        <li class="nav__menu__item">
+          <a href="/blog/">Blog</a>
+        </li>
+      </ul>
+      <div
+        class="toggle"
+        data-toggle={toggle}
+        onClick={() => setToggle((prevState) => !prevState)}
+      >
+        <span class="toggle__bar"></span>
+        <span class="toggle__bar"></span>
+        <span class="toggle__bar"></span>
+      </div>
+    </>
   );
 };
 
